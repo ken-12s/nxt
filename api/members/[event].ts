d7aa029b4af08d4d34973e236030f064e7215e40
ken-12s <junoyu1226@gmail.com>
2025-09-19 13:44:13 +0900

    Update [event].ts

--- api/members/[event].ts
+++ api/members/[event].ts
@@ -2,8 +2,6 @@
 import { createClient } from '@supabase/supabase-js';
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export const config = { runtime: 'nodejs20.x' };
-
 const supabase = createClient(
   process.env.SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
